96e94c5b7f64d16cd382b497bb62a40cdfc601b7
pan.arno@gmail.com <pan.arno@gmail.com>
2025-02-28 17:40:17 -0800

    completed styles in accordions. Toggle works. OnClick handled. Use map and spread to change specific accordion object.

--- react-accordion/src/App.jsx
+++ react-accordion/src/App.jsx
@@ -14,36 +14,36 @@ function App() {
       'button' : false,
     },
     {
-      'question' : 'What are the two most common mobile operating systems?',
-      'answer' : `Google has Android and Apple has the iOS.`,
+      'question' : 'Where are the task bars for the main operating systems?',
+      'answer' : `Windows and Chrome OS is at the bottom. Apple has it at the top. Linux varies.`,
       'button' : false,
     },
     {
-      'question' : 'What are the two most common mobile operating systems?',
-      'answer' : `Google has Android and Apple has the iOS.`,
+      'question' : 'What popular mobile operating systems once dominated the market?',
+      'answer' : `The Blackberry OS by RIM (Blackberry), Nokia's Symbian OS, later Meego OS and after MS's acquried it Windows Mobile OS.`,
       'button' : false,
     },
     {
-      'question' : 'What are the two most common mobile operating systems?',
-      'answer' : `Google has Android and Apple has the iOS.`,
+      'question' : 'What is the current most popular cellular generation?',
+      'answer' : `The latest standard used as of 2025 is 5G. As for popularity it depends on which part of the world you may be from. 4G could be said is the most common.`,
       'button' : false,
     },
   ])
 
   return (
     <>
-      <div>
-        <h1>Accordions</h1>
+      <div className='container'>
+        <h1>Accordions | FAQ for Operating Systems</h1>
         {
           accordions.map((accordion, index) => (
-            <div key={index} style={{'display' : 'flex', 'flexDirection' : 'column'}}>
-              <div style={{'display:' : 'flex', "flexDirection" : 'row', 'justifyContent' : 'space-between', 'alignContent' : 'center'}}>
-                <h2 style={{'textAlign' : 'start', 'margin' : '0'}}>{accordion.question}</h2>
+            <div className="card" key={index}>
+              <div className="question">
+                <h3>{(index + 1)}. {accordion.question}</h3>
                 <button onClick={() => setAccordions((previousAccordions) => previousAccordions.map((a, i) => (
                   i === index ? {...a, button : !a.button} : a
                 )))}>+</button>
               </div>
-              {accordion.button && <p style={{'textAlign' : 'start'}}>{accordion.answer}</p>}
+              {accordion.button && <p className='answer'>{accordion.answer}</p>}
             </div>
           ))
         }
